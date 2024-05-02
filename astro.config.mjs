6c92031aa1b57209477643661ebf0e395c23266f
orlandobricenob <orlando.briceno.blanco@gmail.com>
2024-05-02 16:30:38 -0400

    build: Added lightningcss minifier

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,30 @@
-import { defineConfig } from "astro/config"
-import cloudflare from "@astrojs/cloudflare"
+import cloudflare from '@astrojs/cloudflare'
+import { defineConfig } from 'astro/config'
 
-import sitemap from "@astrojs/sitemap"
+import sitemap from '@astrojs/sitemap'
 
 // https://astro.build/config
 export default defineConfig({
-	output: "server",
-	adapter: cloudflare({
-		imageService: "passthrough",
-	}),
-	site: "https://gadmin.app",
-	integrations: [sitemap()],
+  build: {
+    inlineStylesheets: 'always',
+  },
+  compressHTML: true,
+  prefetch: true,
+  devToolbar: {
+    enabled: false,
+  },
+  output: 'server',
+  adapter: cloudflare({
+    imageService: 'passthrough',
+  }),
+  site: 'https://gadmin.app',
+  integrations: [sitemap()],
+  vite: {
+    build: {
+      cssMinify: 'lightningcss',
+    },
+    ssr: {
+      noExternal: ['path-to-regexp'],
+    },
+  },
 })
